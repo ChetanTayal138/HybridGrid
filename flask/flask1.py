4950fcfad59365243aa5998668afb878f6d84aa5
Harika Balusa <48611563+harikabalusa@users.noreply.github.com>
2019-11-06 13:55:47 +0530

    Added FLASK files
    Chetan is hot

---
+++ flask/flask1.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, url_for, flash, redirect,request
+from forms import LoginForm
+import os
+import mysql.connector
+
+# mydb=mysql.connector.connect(
+#      host="localhost",
+#     user="root",  
+#     passwd="7338330380",
+#     database="uttar_pradesh"
+#     )
+# cursor=mydb.cursor()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@app.route("/login", methods=['GET','POST'])
+
+
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        connection = mysql.connector.connect( host="localhost",
+    user="root",  
+    passwd="7338330380",
+    database="uttar_pradesh")
+        cursor = connection.cursor(buffered = True)
+        a_email=form.email.data
+        a_pwd = form.password.data
+        
+
+#        x=("select * from generators where GEN_ID=3");
+ #       cursor.execute(x)
+  #      a=cursor.fetchall()
+   #     print(a)
+        print(a_email)
+        #checkpwd =( "SELECT password FROM logincheck WHERE email_id ='%s'",(a_email,))
+        #print(checkpwd)
+        cursor.execute("SELECT password FROM logincheck WHERE email_id =%s",(a_email,))
+        pwdcheck=cursor.fetchone()
+        print(pwdcheck[0])
+
+
+       # out = [item for t in pwdcheck for item in t] 
+
+       # print(out)
+
+       # pwdcheck=""
+       # for i in range(0,len(out)):
+       #     pwdcheck+=out[i]
+       # print(pwdcheck)
+      #  print(pwdcheck.replace('(', ''))
+      #  print(pwdcheck.replace(')', ''))
+      #  print(pwdcheck.replace("'", ''))
+      #  print(pwdcheck.replace(',', ''))
+      
+
+
+        if pwdcheck[0] == a_pwd :
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('admin'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/admin")
+def admin():
+    return render_template('admin.html',title='Admin')
+
+if __name__ == '__main__':
+    app.run(debug=True)
