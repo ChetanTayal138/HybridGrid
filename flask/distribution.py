c5c29849f6996fd15edd92fd95d02774f2ce9f99
Chetan Tayal <chetantayal138@gmail.com>
2019-11-28 22:07:36 +0530

    Update distribution.py

--- flask/distribution.py
+++ flask/distribution.py
@@ -5,8 +5,8 @@ from mysqldb import get_query
 
 HOST = 'localhost'
 DATABASE = 'uttar_pradesh'
-USER = 'rhino'
-PASSWORD = 'iamaboy3801'
+USER = ''
+PASSWORD = ''
 UP_URL = "http://www.upsldc.org/real-time-data"
 
 def distribution_main():
